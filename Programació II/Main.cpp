565f0f883241a78acdbce52b470f0bbf2bd12801
Aleix Bueso <aleix.bueso@gmail.com>
2015-11-23 14:06:11 +0100

    23/11

--- Programació II/Main.cpp
+++ Programació II/Main.cpp
@@ -26,10 +26,15 @@ int main()
 	list.PushBack(3);
 	list.PushBack(4);
 	list.PushFront(5);
+	list.Insert(8, 3);
+	list.Remove(0);
 
 	cout << "Count: " << list.Count() << endl;
 	cout << "First: " << list.GetFirst()->data << endl;
 	cout << "Pop Last: " << list.PopBack() << endl;
+	cout << "Count: " << list.Count() << endl;
+	cout << "Pop Front: " << list.PopFront() << endl;
+	cout << "Count: " << list.Count() << endl;
 
 	cout << endl;
 	cout << endl;
